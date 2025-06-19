2e4f5fe108885caf68a1724492482e2ee6a8b60e
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-06-20 01:09:39 +0600

    Simaple Abstruction

---
+++ Module-4/1_inheritance.dart
@@ -0,0 +1,28 @@
+class Person{
+  String name;
+   int age;
+
+   Person(this.name, this.age){
+    print("Person Constructor is Called");
+   }
+
+   void displayInfo(){
+    print("Name : $name, Age : $age");
+   }
+}
+class Student extends Person{
+
+  String school;
+  Student(String name, int age, this.school): super(name, age){
+    print("Student Construcotr is Called");
+  }
+  void showDetails(){
+    displayInfo();
+    print("School Name : $school");
+  }
+
+}
+void main(){
+  Student student = Student("Majid Bhuiyan", 25, "Jawar School ");
+  student.showDetails();
+}
---
+++ Module-4/2_abstruction.dart
@@ -0,0 +1,41 @@
+abstract class Animal{
+    String name;
+    String food;
+    String sound;
+    Animal(this.name, this.food, this.sound);
+
+    eat(){
+      print("$name is Eating $food");
+    }
+    makeSound(){
+      print("$name is making sound $sound");
+    }
+}
+class Dog extends Animal{
+  String color;
+
+  Dog(String name, String food, String sound, this.color):super(name, food, sound);
+  void colorInfo(){
+      print("$name color is $color");
+  }
+}
+
+class Cat extends Animal{
+  String color;
+
+  Cat(String name, String food, String sound, this.color):super(name, food, sound);
+  void colorInfo(){
+      print("$name color is $color");
+  }
+}
+void main(){
+  Dog dog = Dog("Shiku Dog", "Gorur Haddi", "Gheo Gheo", "Black White");
+  dog.eat();
+  dog.makeSound();
+  dog.colorInfo();
+
+  Cat cat = Cat("Mitu Pussy", "Macher Kata", "Mew Mew", "White");
+  cat.eat();
+  cat.makeSound();
+  cat.colorInfo();
+}
