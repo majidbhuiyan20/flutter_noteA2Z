998f0255686ccf91935fa1d4c2c16c3c71f9e6bf
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-07-02 01:42:48 +0600

    Update Code

--- test.dart
+++ test.dart
@@ -3,30 +3,31 @@ class Person{
     int age;
 
     Person(this.name, this.age){
-        print("Person Constructor is called");
-    }
+        print("Person Constructor is Called");
 
-    void displayInfo(){
-        print("Name : $name, Age : $age");
+    }
+    void displayPerson(){
+        print("Name is : $name, Age : $age");
     }
 }
 
 class Student extends Person{
 
     String school;
-    Student(String name, int age, this.school): super(name, age){
-        print("Student constructor is Called");
+
+    Student(String name, int age, this.school ):super(name, age){
+        print("Student Constructor is Called" );
+
+
     }
-    void showDetails(){
-        displayInfo();
-        print("School name : $school");
+    void displayStudent(){
+        displayPerson();
+        print("School name is : $school");
     }
 }
 
 void main(){
-    Student student = Student("Majid Bhuiyan", 23, "Jawar High School");
-    student.showDetails();
-    Person person = Person("Majid Bhuiyan", 23);
-    person.displayInfo();
 
+    Student std = Student("Majid Bhuiyan", 23, "ABC School" );
+    std.displayStudent();
 }
