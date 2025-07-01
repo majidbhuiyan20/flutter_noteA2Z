86f7e62db9decc3120a0e16fd112a79108c85c1d
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-07-02 01:42:48 +0600

    Update Code

---
+++ Modele-6/git_command.dart
@@ -0,0 +1,25 @@
+//All Github Command
+
+// git config --global user.name "Your Name"
+// git config --global user.email "you@example.com"
+// git config --list   # View config
+
+
+// git init                        # Initialize a new Git repo
+// git clone <repo_url>            # Clone existing repo
+
+
+// git status                      # See current changes
+// git add <file>                  # Stage a file
+// git add .                        # Stage all changes
+// git commit -m "Message"          # Commit staged changes
+// git rm <file>                    # Remove file and stage
+// git mv <old> <new>               # Rename file
+
+
+// git branch                      # List branches
+// git branch <name>               # Create branch
+// git checkout <name>             # Switch branch
+// git checkout -b <name>          # Create + switch
+// git merge <branch>              # Merge branch
+// git branch -d <name>            # Delete branch
